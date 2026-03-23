49110553e07d0eba696af9f4da59e9a9708de0fb
Manoel <joao.e.souza12@aluno.senai.br>
2026-03-23 08:01:48 -0300

    primeiro commit

---
+++ mvcAluno/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/aluno/listar',[AlunoController::class,'Listar'])->
+name('aluno.listar');
+
+Route::get('/aluno/cadastrar', function(){
+    return view('cadastro');
+})->name('aluno.cadastro');
+
+// POST - enviar os dados para cadastrar usúarios 
+Route::post('/aluno/salvar',[AlunoController::class, 'add'])
+->name('aluno.salvar');
+
