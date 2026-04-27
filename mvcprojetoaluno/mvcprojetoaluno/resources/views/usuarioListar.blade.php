71ce882bcf4bd8e2508fe10733d9d592c6a20b7c
Manoel <joao.e.souza12@aluno.senai.br>
2026-04-27 10:45:33 -0300

    projetoaluno

---
+++ mvcprojetoaluno/mvcprojetoaluno/resources/views/usuarioListar.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Usuarios</title>
+</head>
+<body>
+    <h2>Usuários</h2>
+    <a href="/PHP_PBE_2IDS_2025/mvcprojetoaluno/usuario/telaCadastro">
+        Ir para tela Cadastrar</a>
+    <table border="1">
+        <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Ações</th>
+        </tr>
+        <!-- TELA usuarioListar.php -->
+        <?php foreach($usuarios as $id => $u): ?>
+            <tr>
+                <td><?= $u['nome']?></td>
+                <td><?= $u['email']?></td>
+                <td>
+    <a href="/PHP_PBE_2IDS_2025/mvcprojetoaluno/usuario/telaEditar?id=<?= $id ?>">
+        Editar
+    </a>
+    <a href="/PHP_PBE_2IDS_2025/mvcprojetoaluno/usuario/excluir?id=<?= $id ?>">
+        Excluir
+    </a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
